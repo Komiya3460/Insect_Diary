8fdd64c7f86bf5a0cf47b7ad96e4ae0dc89f6767
komiya3460 <ochi130325@gmail.com>
2024-09-22 15:05:09 +0900

    Initial Commit

---
+++ KonchuuLife/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KonchuuLife
+//
+//  Created by 越智三四郎 on 2024/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KonchuuLife/KonchuuLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KonchuuLifeApp.swift
+//  KonchuuLife
+//
+//  Created by 越智三四郎 on 2024/09/22.
+//
+
+import SwiftUI
+
+@main
+struct KonchuuLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
